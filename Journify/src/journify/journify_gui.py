9fbcc60578beb2e4f5650b906c55b3df53618d6d
Thai Quoc Tran <tranquocthai7@gmail.com>
2024-11-01 00:41:54 +0700

    Update journify_gui.py

--- Journify/src/journify/journify_gui.py
+++ Journify/src/journify/journify_gui.py
@@ -11,7 +11,7 @@ from util.pages.home_page import home_page
 #from util.pages.chatbot_page import chatbot_page
 #from util.pages.supporter_page import supporter_page
 #from util.pages.trendings_page import trendings_page
-#from util.pages.about_page import about_page
+from util.pages.about_page import about_page
 
 
 class MultiApp:
@@ -21,15 +21,15 @@ class MultiApp:
     def add_app(self, title, func):
         self.apps.append({"title": title, "function": func})
 
-    def run(self):
+    '''def run(self):
         img = Image.open(
             get_file_path(
                 "rascore_logo.png",
                 dir_path=f"{get_dir_name(__file__)}/{util_str}/{data_str}",
             ),
-        )
+        )'''
 
-        st.set_page_config(page_title="rascore", page_icon=img, layout="wide")
+        st.set_page_config(page_title="Journify", page_icon=img, layout="wide")
 
         st.sidebar.markdown("## Main Menu")
         app = st.sidebar.selectbox(
@@ -47,6 +47,6 @@ app.add_app("Home Page", home_page)
 #app.add_app("Q&A Chat Bot", chatbot_page)
 #app.add_app("Submission Supporter", supporter_page)
 #app.add_app("Research Trendings", trendings_page)
-#app.add_app("About Us", about_page)
+app.add_app("About Us", about_page)
 
 app.run()
