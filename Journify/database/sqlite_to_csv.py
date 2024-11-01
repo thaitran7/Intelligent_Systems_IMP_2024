65e51caa1ecdb58d121cde46b42aead3113c1026
Thai Quoc Tran <tranquocthai7@gmail.com>
2024-11-01 10:39:58 +0700

    Update sqlite_to_csv.py

--- Journify/database/sqlite_to_csv.py
+++ Journify/database/sqlite_to_csv.py
@@ -1,7 +1,7 @@
 from database.json_to_sqlite import query_all_data
 import pandas as pd
 
-csv_file_path = "database/arxiv_data.csv"  # Desired CSV file path
+csv_file_path = "Journify/database/arxiv_data.csv"  # Desired CSV file path
 
 def export_to_csv(csv_file_path=csv_file_path):
     df = pd.DataFrame(query_all_data())
